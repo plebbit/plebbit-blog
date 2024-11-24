9f53ead2e1b0551b096aee6bd52b145fb4bf2db7
Tom (plebeius.eth) <tom@plebbit.com>
2024-11-24 16:00:13 +0100

    init

---
+++ src/app.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './views/home';
+import PostPage from './views/post-page';
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/post/:id" element={<PostPage />} />
+    </Routes>
+  );
+};
+
+export default App;
---
+++ src/views/post-page/post-page.tsx
@@ -0,0 +1,7 @@
+import styles from './post-page.module.css';
+
+const PostPage = () => {
+  return <div className={styles.postPage}>PostPage</div>;
+};
+
+export default PostPage;
