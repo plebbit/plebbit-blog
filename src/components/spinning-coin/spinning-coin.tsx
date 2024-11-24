531a6b3811dd19510eeb19b787b4441c9b59a634
Tom (plebeius.eth) <tom@plebbit.com>
2024-11-24 22:25:41 +0100

    add spinning logo

---
+++ src/components/spinning-coin/spinning-coin.tsx
@@ -0,0 +1,26 @@
+import styles from './spinning-coin.module.css';
+
+const SpinningCoin = () => {
+  return (
+    <div className={styles.coinContainer}>
+      <svg className={styles.coin} version="1.1" viewBox="-5 -5 316.14 316.14" xmlns="http://www.w3.org/2000/svg">
+        <circle 
+          cx="153.07" 
+          cy="153.07" 
+          r="151.07" 
+          fill="#000" 
+          stroke="#e9e76c" 
+          strokeWidth="2"
+        />
+        <g transform="translate(-229.37 -308.69)">
+          <circle cx="334.04" cy="399.32" r="14.563" fill="#e9e76c"/>
+          <circle cx="476.01" cy="400.8" r="14.563" fill="#e9e76c"/>
+          <path d="m427.18 415.83 26.214 97.087-80.583-1.9418" fill="none" stroke="#e9e76c" strokeLinecap="round" strokeLinejoin="round" strokeWidth="13.5"/>
+          <path d="m359.22 557.57 100.97 3.8835" fill="none" stroke="#e9e76c" strokeLinecap="round" strokeWidth="13.5"/>
+        </g>
+      </svg>
+    </div>
+  );
+};
+
+export default SpinningCoin;
--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -2,6 +2,7 @@ import { useFeed } from '@plebbit/plebbit-react-hooks';
 import styles from './home.module.css';
 import Header from '../../components/header';
 import FeedPost from '../../components/feed-post';
+import SpinningCoin from '../../components/spinning-coin';
 
 const Home = () => {
   const { feed } = useFeed({subplebbitAddresses: ['blog.plebbit.eth']});
@@ -9,9 +10,13 @@ const Home = () => {
   return (
     <div className={styles.home}>
       <Header />
-      <div className={styles.hero} />
-      {feed?.map((post) => (
-        <FeedPost post={post} key={post.cid} />
+      <div className={styles.hero}>
+        <SpinningCoin />
+      </div>
+      {Array(100).fill(null).map(() => (
+        feed?.map((post) => (
+          <FeedPost post={post} key={post.cid} />
+        ))
       ))}
     </div>
   );
