7308838d7a03f666ef22888c9c3f8673311f028c
Tom (plebeius.eth) <tom@plebbit.com>
2024-11-25 13:30:51 +0100

    add markdown

---
+++ src/components/markdown/markdown.tsx
@@ -0,0 +1,42 @@
+import styles from './markdown.module.css';
+import ReactMarkdown from 'react-markdown';
+import rehypeSanitize from 'rehype-sanitize';
+import remarkGfm from 'remark-gfm';
+import supersub from 'remark-supersub';
+
+interface MarkdownProps {
+  content: string;
+}
+
+const MAX_LENGTH_FOR_GFM = 10000; // remarkGfm lags with large content
+
+const Markdown = ({ content }: MarkdownProps) => {
+  const remarkPlugins: any[] = [[supersub]];
+
+  if (content.length <= MAX_LENGTH_FOR_GFM) {
+    remarkPlugins.push([remarkGfm, { singleTilde: false }]);
+  }
+
+  return (
+    <span className={styles.markdown}>
+      <ReactMarkdown
+        children={content}
+        remarkPlugins={remarkPlugins}
+        rehypePlugins={[[rehypeSanitize]]}
+        components={{
+          a: ({ children, href }) => (
+            <a href={href} target='_blank' rel='noopener noreferrer'>
+              {children}
+            </a>
+          ),
+          img: ({ src }) => <span>{src}</span>,
+          video: ({ src }) => <span>{src}</span>,
+          iframe: ({ src }) => <span>{src}</span>,
+          source: ({ src }) => <span>{src}</span>,
+        }}
+      />
+    </span>
+  );
+};
+
+export default Markdown;
--- src/views/post-page/post-page.tsx
+++ src/views/post-page/post-page.tsx
@@ -2,6 +2,7 @@ import { useComment } from '@plebbit/plebbit-react-hooks';
 import styles from './post-page.module.css';
 import { useParams } from 'react-router-dom';
 import { formatLocalizedUTCTimestamp } from '../../lib/time-utils';
+import Markdown from '../../components/markdown';
 
 const PostPage = () => {
   const commentCid = useParams().commentCid;
@@ -12,7 +13,7 @@ const PostPage = () => {
     return <div>This is not the blog subplebbit</div>;
   }
 
-  const loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+  const loremIpsum = 'Lorem ipsum dolor *sit amet*, **consectetur** adipiscing [elit](https://www.google.com).\n\nSed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\n\n&nbsp;\n\n Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
   return (
     <div className={styles.postPage}>
@@ -26,7 +27,7 @@ const PostPage = () => {
           <span className={styles.separator} />
           <span className={styles.comments}>{replyCount} {replyCount === 1 ? 'reply' : 'replies'}</span>
         </div>
-        <p>{content || loremIpsum}</p>
+        <Markdown content={content || loremIpsum} />
       </div>
     </div>
   );
