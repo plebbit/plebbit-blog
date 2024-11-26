2e301ffd66301b5a2b8d9eb7056118e73d159d3b
Tom (plebeius.eth) <tom@plebbit.com>
2024-11-26 12:54:38 +0100

    add feed state string

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -2,19 +2,21 @@ import { useFeed } from '@plebbit/plebbit-react-hooks';
 import styles from './home.module.css';
 import FeedPost from '../../components/feed-post';
 import SpinningCoin from '../../components/spinning-coin';
+import useFeedStateString from '../../hooks/use-feed-state-string';
+import LoadingEllipsis from '../../components/loading-ellipsis';
 
 const Home = () => {
   const { feed } = useFeed({subplebbitAddresses: ['blog.plebbit.eth']});
+  const stateString = <LoadingEllipsis string={useFeedStateString(['blog.plebbit.eth']) || 'loading'} />;
 
   return (
     <div className={styles.home}>
       <div className={styles.hero}>
         <SpinningCoin />
       </div>
-      {Array(10).fill(null).map(() => (
-        feed?.map((post) => (
-          <FeedPost post={post} key={post.cid} />
-        ))
+      {feed.length === 0 && <div className={styles.stateString}>{stateString}</div>}
+      {feed?.map((post) => (
+        <FeedPost post={post} key={post.cid} />
       ))}
     </div>
   );
